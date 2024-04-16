5d04e8b9afb539d71075ab82f1d4c2e338dbc83f
mihinduranasinghe <mihinduranasinghe@gmail.com>
2024-04-16 18:11:49 +0200

    Updating career and educational background

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,28 +4,39 @@ import * as awesom from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-  mapMarker: any ;
-  building:any;
-  mobile:any;
-  email:any;
-  birthday:any;
-  badge:any;
-  home:any;
-  constructor() {
-    
-   }
+  mapMarker: any;
+  building: any;
+  mobile: any;
+  email: any;
+  birthday: any;
+  badge: any;
+  home: any;
+  age!: number;
+  constructor() {}
 
   ngOnInit(): void {
-    this.mapMarker=awesom.faMapMarker;
-    this.building=awesom.faCity;
-    this.email=awesom.faMailBulk;
-    this.mobile=awesom.faMobile;
-    this.birthday=awesom.faBirthdayCake;
-    this.badge=awesom.faGraduationCap;
-    this.home=awesom.faLaptopHouse;
+    this.mapMarker = awesom.faMapMarker;
+    this.building = awesom.faCity;
+    this.email = awesom.faMailBulk;
+    this.mobile = awesom.faMobile;
+    this.birthday = awesom.faBirthdayCake;
+    this.birthday = awesom.faBirthdayCake;
+    this.badge = awesom.faGraduationCap;
+    this.home = awesom.faLaptopHouse;
+    this.calculateAge(new Date(1997, 9, 4));
   }
 
+  calculateAge(dob: Date): void {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    this.age = age;
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,27 +4,25 @@ import Typed from 'typed.js';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  // avatar= 'assets/images/avatar.png';
-  // avatar= 'assets/images/avatar2.png';
+  avatar = 'assets/images/avatar.jpg';
+  // avatar = 'assets/images/avatar2.jpg';
   // avatar= 'assets/images/Mihindu Ranasinghe2.jpg';
-  avatar= 'assets/images/Mihindu Ranasinghe3.jpg';
+  // avatar= 'assets/images/Mihindu Ranasinghe3.jpg';
 
   ngOnInit(): void {
     var options = {
-      strings: ['','Full-Stack Engineer', 'Machine Learning Engineer','Cloud Native Enthusiast'],
+      strings: ['', 'Full-Stack Engineer', 'Machine Learning & AI Enthusiast'],
       typeSpeed: 50,
       backSpeed: 50,
       loop: true,
     };
-    
-    var typed = new Typed('.typed', options);
-    typed.reset(true)
-  }
 
+    var typed = new Typed('.typed', options);
+    typed.reset(true);
+  }
 }
